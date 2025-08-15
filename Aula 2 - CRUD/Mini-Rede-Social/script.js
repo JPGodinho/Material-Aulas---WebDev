237ed1e6377f73a1f56402f1e08d9dd7986751a3
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-14 22:31:47 -0300

    aula 2

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -18,3 +18,63 @@ let posts = [
         date: "12/10/2023 12:00:00"
     }
 ];
+
+window.onload = function(){
+    mostrarPosts();
+
+    document.querySelector("#postForm").addEventListener("submit", addPost)
+}
+
+
+// Create
+function addPost(e){
+    e.preventDefault();
+
+    const textoPost = document.querySelector("#postText").value;
+    const categoriaPost = document.querySelector("#postCategory").value;
+    const imagemPost = document.querySelector("#postImage").value;
+
+    const postNovo = {
+        text: textoPost,
+        category: categoriaPost,
+        image: imagemPost,
+        date: new Date().toLocaleString()
+    }
+
+    posts.unshift(postNovo)
+
+    mostrarPosts()
+}
+
+// Read
+function mostrarPosts(){
+    // pegando o elemento onde os tweets estão sendo inseridos
+    const listaPosts = document.querySelector("#postList")
+    listaPosts.innerHTML = ""
+
+    // passando pelo array e criando um tweet para cada um
+    posts.forEach(pegaItem => {
+        const cardPost = document.createElement("div")
+        cardPost.classList.add("card")
+
+        cardPost.innerHTML = `
+            <h2>${pegaItem.text}</h2>
+            <img src="${pegaItem.image}"/>
+            <p>Categoria: ${pegaItem.category}</p>
+            <p>Data: ${pegaItem.date}</p>
+            <button>Editar</button> <button>Apagar</button>
+        `
+
+        listaPosts.append(cardPost)
+    })
+}
+
+// Update
+function editarPosts(){
+
+}
+
+// Delete
+function excluirPosts(){
+
+}
