cd3cb0515bb9c900b65351b248a0077138c1023f
Joao Pedro Godinho Passiani <rm561602@fiap.com.br>
2025-08-28 22:41:12 -0300

    react

---
+++ react-project/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Header from './components/Header'
+
+
+function App() {
+
+  return (
+    <>
+      <Header 
+        titulo="Gui Negão"
+        texto = "o maior"/>
+      <Header 
+        titulo="Matheuzinho"/>
+      <Header 
+        titulo="Hugo Souza"/>
+    </>
+  )
+}
+
+export default App
---
+++ react-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header(props){
+    return(
+        <>
+            <h1>{props.titulo}</h1>
+            <p>
+                {props.texto}
+            </p>
+        </>
+    )
+}
+
+export default Header
